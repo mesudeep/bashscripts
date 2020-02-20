c0042f07061b6e306eac9d8674ff2c44c386a752
Ramesh Bishwakarma <rameshbishwakarma@Rameshs-MBP.lan1>
2020-02-19 23:50:41 -0600

    Added Jenkinsfiles

---
+++ jenkinsfiles/jenkinsfiles/MyClass.cs
@@ -0,0 +1,19 @@
+﻿node {}{
+    Stage(‘checkout’)
+{
+    Checkout sim
+}
+stage(‘Build’)
+{
+    Sh “sh untilloop.sh”
+}
+Stage(‘Array’)
+{
+    Sh “sh array”
+}
+Stage(Notify”)
+{
+    Sh ‘mail - s “the job ran fine” sanjaysunar933 @gmail.com’
+
+    }
+}
